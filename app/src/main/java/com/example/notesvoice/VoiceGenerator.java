c31937239f9dde7140595f5093edf66b2745f74b
grpfv <academease.app@gmail.com>
2024-02-15 04:08:26 +0800

    Updated

--- app/src/main/java/com/example/notesvoice/VoiceGenerator.java
+++ app/src/main/java/com/example/notesvoice/VoiceGenerator.java
@@ -1,11 +1,15 @@
 package com.example.notesvoice;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
@@ -14,14 +18,32 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class VoiceGenerator extends AppCompatActivity {
 
     private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
+    private static final int REQUEST_CODE_CAMERA_PERMISSION = 1001;
+    private static final int REQUEST_CODE_PICK_IMAGE = 1002;
+
     EditText titleEditText, notesEditText;
     Button addButton;
+    ImageView micImageView, cameraImageView;
+
+    TextRecognizer textRecognizer;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,59 +53,51 @@ public class VoiceGenerator extends AppCompatActivity {
         titleEditText = findViewById(R.id.Title);
         notesEditText = findViewById(R.id.notes);
         addButton = findViewById(R.id.savebutton);
-        ImageView micImageView = findViewById(R.id.mic);
+        micImageView = findViewById(R.id.mic);
+        cameraImageView = findViewById(R.id.camera);
 
         micImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Check permission
-                if (checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
-                    // Permission denied, request it
-                    String[] permissions = {Manifest.permission.RECORD_AUDIO};
-                    requestPermissions(permissions, REQUEST_CODE_SPEECH_INPUT);
-                } else {
-                    // Permission already granted
-                    startSpeechToText();
-                }
+                startSpeechToText();
+            }
+        });
+
+        cameraImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCameraPermission();
             }
         });
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get text from title and notes EditText
                 String titleText = titleEditText.getText().toString().trim();
                 String notesText = notesEditText.getText().toString().trim();
 
-                // Check if either title or notes are empty
                 if (!titleText.isEmpty() && !notesText.isEmpty()) {
-                    // Create intent to navigate to Notelist activity
                     Intent intent = new Intent(VoiceGenerator.this, Notelist.class);
-                    // Pass title and notes text as extras
                     intent.putExtra("title", titleText);
                     intent.putExtra("notes", notesText);
-                    // Start Notelist activity
                     startActivity(intent);
-
-                    // Show toast message
                     Toast.makeText(VoiceGenerator.this, "Notes saved", Toast.LENGTH_SHORT).show();
                 } else {
-                    // Show toast message if title or notes are empty
                     Toast.makeText(VoiceGenerator.this, "Title or notes are empty!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+        // Initialize TextRecognizer
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
     }
 
-    // Start speech to text intent
     private void startSpeechToText() {
-        // Intent to show speech to text dialog
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...");
 
-        // Start intent
         try {
             startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
         } catch (Exception e) {
@@ -91,39 +105,69 @@ public class VoiceGenerator extends AppCompatActivity {
         }
     }
 
-    // Receive speech input result
+    private void checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA_PERMISSION);
+        } else {
+            openImagePicker();
+        }
+    }
+
+    private void openImagePicker() {
+        ImagePicker.with(this)
+                .crop()	     //Crop image(Optional), Check Customization for more option
+                .compress(1024)	 //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080) //Final image resolution will be less than 1080 x 1080(Optional)
+                .start(REQUEST_CODE_PICK_IMAGE);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case REQUEST_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
-                    // Get text array from voice intent
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    // Set text to notesEditText
-                    if (result != null && result.size() > 0) {
-                        notesEditText.setText(result.get(0));
-                    }
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && null != data) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if (result != null && !result.isEmpty()) {
+                    notesEditText.setText(result.get(0));
                 }
-                break;
+            }
+        } else if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            recognizeTextFromImage();
+        }
+    }
+
+    private void recognizeTextFromImage() {
+        if (imageUri != null) {
+            try {
+                InputImage inputImage = InputImage.fromFilePath(VoiceGenerator.this, imageUri);
+                Task<Text> result = textRecognizer.process(inputImage).addOnSuccessListener(new OnSuccessListener<Text>() {
+                    @Override
+                    public void onSuccess(Text text) {
+                        String recognizedText = text.getText();
+                        recognizedText = recognizedText.replace("\n", " ");
+                        notesEditText.setText(recognizedText);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(VoiceGenerator.this, "Text recognition failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (IOException e) {
+                Toast.makeText(VoiceGenerator.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    // Handle permission result
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_CODE_SPEECH_INPUT: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission granted
-                    startSpeechToText();
-                } else {
-                    // Permission denied
-                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
-                }
-                break;
+        if (requestCode == REQUEST_CODE_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openImagePicker();
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
             }
         }
     }
